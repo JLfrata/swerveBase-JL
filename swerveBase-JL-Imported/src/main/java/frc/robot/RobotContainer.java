99ca24dbbb8932b85bc4fc781ddfb7282beaa00f
DevilLuiz <luizguiforest@gmail.com>
2026-05-27 20:02:39 -0300

    Codigo final

---
+++ swerveBase-JL-Imported/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.subsystems.SwerveSubsystem;
+import swervelib.SwerveInputStream;
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+public class RobotContainer {
+
+  private final SwerveSubsystem swerveBase = new SwerveSubsystem();
+  private final PS4Controller controller = new PS4Controller(Constants.OperatorConstants.kDriverControllerPort);
+
+  public RobotContainer() {
+    configureBindings();
+  }
+
+  // SwerveInputStream driveAngularVelocity = SwerveInputStream.of(swerveBase.getSwerveDrive(),
+  //     () -> controller.getLeftY() * -0.8,
+  //     () -> controller.getLeftX() * -0.8)
+  //     .withControllerRotationAxis(controller::getRightX)
+  //     .deadband(0.05)
+  //     .scaleTranslation(0.8)
+  //     .allianceRelativeControl(true);
+
+  // SwerveInputStream driveDirectAngle = driveAngularVelocity.copy().withControllerHeadingAxis(controller::getRightX,
+  //     controller::getRightY)
+  //     .headingWhile(true);
+      
+  //     Command driveFieldOrientedDirectAngle = swerveBase.driveFieldOriented(driveDirectAngle);
+  //     Command driveFieldOrientedDirectAngle = swerveBase.driveFieldOriented(driveDirectAngle);
+      
+  private void configureBindings() {
+    swerveBase.setDefaultCommand(swerveBase.driveTeleOp(
+      controller::getLeftX,
+      controller::getLeftY,
+      controller::getRightX));
+    new JoystickButton(controller, PS4Controller.Button.kCross.value).onTrue(swerveBase.resetGyro()); 
+  }
+
+  public Command getAutonomousCommand() {
+    return null;
+  }
+}
